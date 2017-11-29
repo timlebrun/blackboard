8f2a9b8d8bc26151c2ce59b0b0c39ac4dbfdc34c
Tim <me@timlebrun.fr>
2017-11-29 15:10:23 +0100

    Adding error pages view

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+    <h1>
+        Oops
+        <small class="d-block">You can't go here</small>
+    </h1>
+
+
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+    <h1>
+        Oops 404
+        <small class="d-block">There is nothing here</small>
+    </h1>
+
+
+@endsection
